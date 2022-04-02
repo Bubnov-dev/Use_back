f3a8814293ad7673bcf28693bdc42ae852f9f243
Sascha <sasha.bubnov.k@gmail.com>
2022-04-02 10:55:17 +0300

    add some columns

---
+++ app/Http/Controllers/ClientProfileController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Client_profile;
+use Illuminate\Http\Request;
+
+class ClientProfileController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Client_profile  $client_profile
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Client_profile $client_profile)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Client_profile  $client_profile
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Client_profile $client_profile)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Client_profile  $client_profile
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Client_profile $client_profile)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Client_profile  $client_profile
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Client_profile $client_profile)
+    {
+        //
+    }
+}
