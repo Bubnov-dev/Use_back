e75783467956365de6db3f455adbee325c1bf143
Sascha <sasha.bubnov.k@gmail.com>
2022-04-13 18:08:22 +0300

    start auth

--- app/Http/Controllers/Business/AuthBusinessController.php
+++ app/Http/Controllers/Business/AuthBusinessController.php
@@ -3,14 +3,49 @@
 namespace App\Http\Controllers\Business;
 
 use App\Http\Controllers\Controller;
+use App\Models\Business\BusinessUser;
+use App\Models\Business\BusinessUserOwner;
+use App\Models\Business\Company;
+use App\Models\Business\CompanyCategory;
+use App\Models\Business\Representative;
 use Illuminate\Http\Request;
 
 class AuthBusinessController extends Controller
 {
-    public function register(Request $request){
-//        $user = BusinessUser::create([
-//           'name' => $request->input('name')
-//           'email'
-//        ]);
+    public function registerOwner(Request $request)
+    {
+        $user = BusinessUser::create([
+            'email' => $request->input('email'),
+            'phone' => $request->input('phone'),
+            'password' => $request->input('password')
+        ]);
+
+        $user->save();
+
+        return BusinessUserOwner::create([
+            'business_user_id' => $user->id
+        ])->save();
+    }
+
+    public function addCompany(Request $request)
+    {
+        $company = Company::create([
+            'name' => $request->input('name'),
+            'description' => $request->input('description'),
+            'logo' => $request->input('logo'),
+            'company_category_id' => $request->input('company_category_id')
+        ]);
+        $company->save();
+        foreach ($request->input('representatives') as $representative) {
+            $_representative = Representative::create([
+                'address' => $representative->address,
+                'howToGo' => $representative->howToGo,
+                'phone' => $representative->phone
+            ]);
+
+            $_representative->save();
+
+
+        }
     }
 }
--- database/migrations/2022_04_13_140748_update_business_user_delete_name.php
+++ database/migrations/2022_04_13_140748_update_business_user_delete_name.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class UpdateBuisinessUserDeleteName extends Migration
+class UpdateBusinessUserDeleteName extends Migration
 {
     /**
      * Run the migrations.
